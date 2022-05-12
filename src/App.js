84a8e0457e205316407c2eeaf565d4c7ac4073d0
sofiiraciti <soysofiaraciti@gmail.com>
2022-05-12 13:27:47 -0300

    algo funcionando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ViewsContainer from "./modules/Views";
+// import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import ViewsContainer from "./modules/Views";
 
-const App = () => (
-  <BrowserRouter>
-    <ViewsContainer />
-  </BrowserRouter>
-);
+// const App = () => (
+//   <BrowserRouter>
+//     <ViewsContainer />
+//   </BrowserRouter>
+// );
 
-export default App;
+//export default App;
 
+import React from "react"
+import ContentContainer from "./components/ContentContainer"
+import Title from "./components/Title"
+import TopBar from "./components/TopBar"
+import {BrowserRouter} from "react-router-dom"
+import ViewsContainer from "./modules/Views"
+import Theme from "./Theme"
+import {ThemeStore} from "./contexts/ThemeStore"
+
+const App = () => {
+  return (
+    <ThemeStore>
+      <Theme>
+        <TopBar />
+        <ContentContainer>
+          <Title>THEME SWITCHING WITH STYLED-COMPONENTS</Title>
+          <BrowserRouter>
+            <ViewsContainer />
+          </BrowserRouter>
+        </ContentContainer>
+      </Theme>
+    </ThemeStore>
+  )
+}
+
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const Title = styled.h1` color: ${(props) => props.theme.title}; `;
+
+export default Title;
